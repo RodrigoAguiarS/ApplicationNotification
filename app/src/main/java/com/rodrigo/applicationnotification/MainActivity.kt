28f30fc33d4c80b846077f6653fa5a8c02088501
Rodrigo Aguiar <rodrigo_aguiars@outlook.com>
2021-09-30 10:46:25 -0300

    O que é uma notificação? e preparação inicial do código

---
+++ app/src/main/java/com/rodrigo/applicationnotification/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.rodrigo.applicationnotification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    lateinit var btnSend: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnSend = findViewById(R.id.button_send_notification)
+        btnSend.setOnClickListener {  }
+    }
+}
